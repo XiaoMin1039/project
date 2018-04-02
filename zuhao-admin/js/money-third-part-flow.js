6ceb7627b9de13dffa53386df75440c1c78440c3
xiaomin <13959541039@163.com>
2018-04-02 22:54:13 +0800

    租号平台前台+后台 2期 jq+百度模板引擎

--- zuhao-admin/js/money-third-part-flow.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('#orderIdNotice').mouseenter(function () {
-        $(this).toolTip({content:'可输入交易管理中的订单编号'})
-    });
-    $('#stateNotice').mouseenter(function () {
-        $(this).toolTip({content:'可输入宝贝管理中的产品编号'})
-    });
-});
